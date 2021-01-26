3ee90d21d662b27e47e3b74e3ef596a77b480218
Chinedu Victor <nwachukwuvictorc@gmil.com>
2021-01-26 02:27:44 +0100

    second commit of 2021

---
+++ Web/src/main/java/com/customer/CustomerMainApplication.java
@@ -0,0 +1,14 @@
+package com.customer;
+
+import org.springframework.boot.SpringApplicationRunListener;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerMainApplication {
+
+    public static void main(String[] args){
+
+
+
+    }
+}
